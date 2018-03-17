3e3e84256ef1f11f23700d3e11855a58fedb950d
zhaobe <zhaoben777@gmail.com>
2018-03-17 00:15:36 -0700

    Adding index erb file and switched to MCU voting topic.

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ configure :development do
 end
 
 get '/' do
-	@title = 'Welcome to dinner voting'
+	@title = 'Welcome to Marvel movies voting'
 	erb :index
 end
 
@@ -17,8 +17,22 @@ post '/cast' do
 end
 
 Choices = {
-  'HAM' => 'Hamburger',
-  'PIZ' => 'Pizza',
-  'CUR' => 'Curry',
-  'NOO' => 'Noodles',
+  'IM' => 'Iron Man',
+  'HU' => 'Hulk',
+  'IM2' => 'Iron Man 2',
+  'TH' => 'Thor',
+  'CA' => 'Captain America',
+  'AV' => 'Avengers',
+  'TH2' => 'Thor 2',
+  'IM3' => 'Iron Man 3',
+  'CA2' => 'Captain America 2',
+  'GG' => 'Guardians of the Galaxy',
+  'AV2' => 'Avengers 2',
+  'AM' => 'Ant Man',
+  'CA3' => 'Captain America 3',
+  'DS' => 'Doctor Strange',
+  'GG2' => 'Guardians of the Galaxy 2',
+  'SM' => 'Spider-Man',
+  'TH3' => 'Thor 3',
+  'BP' => 'Black Panther',
 }
